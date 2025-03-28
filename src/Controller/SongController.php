27c5798beb23124d1fe531be076938a72252a9c9
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-28 15:42:02 +0100

    song recovery

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SongRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class SongController extends AbstractController
 {
     #[Route('/song', name: 'app_song')]
-    public function index(): Response
-    {
-        return $this->render('song/index.html.twig');
+    public function index(SongRepository $songRepository): Response
+    {   
+        $songs = $songRepository->findAll();
+
+        return $this->render('song/index.html.twig', [
+            'songs' => $songs,
+        ]);
     }
 }
