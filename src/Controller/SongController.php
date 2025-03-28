a122c895aef7c69db8f7a20971cb664a3bb385af
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-28 16:18:24 +0100

    added forms, need to go through each and correct problems

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Song;
+use App\Form\SongType;
 use App\Repository\SongRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +27,43 @@ final class SongController extends AbstractController
     public function show(Song $song): Response
     {
         return $this->render('song/show.html.twig', [
+            'song' => $song,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_song_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $song = new Song();
+        $form = $this->createForm(SongType::class, $song);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($song);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_song'); // Redirection vers la liste des morceaux
+        }
+
+        return $this->render('song/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'app_song_edit')]
+    public function edit(Request $request, Song $song, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SongType::class, $song);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_song');
+        }
+
+        return $this->render('song/edit.html.twig', [
+            'form' => $form->createView(),
             'song' => $song,
         ]);
     }
--- src/Form/SongType.php
+++ src/Form/SongType.php
@@ -7,6 +7,10 @@ use App\Entity\Song;
 use App\Entity\UserSongKnowledge;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,21 +19,47 @@ class SongType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('lyrics')
-            ->add('isDuo')
-            ->add('isDownloaded')
-            ->add('hasLyrics')
+            ->add('title', TextType::class, [
+                'label' => 'Titre du morceau',
+                'required' => true,
+                'attr' => ['class' => 'form-input'],
+            ])
+            ->add('lyrics', TextareaType::class, [
+                'label' => 'Paroles',
+                'required' => false,
+                'attr' => ['class' => 'form-textarea', 'rows' => 4],
+            ])
+            ->add('isDuo', CheckboxType::class, [
+                'label' => 'Duo ?',
+                'required' => false,
+            ])
+            ->add('isDownloaded', CheckboxType::class, [
+                'label' => 'Téléchargé ?',
+                'required' => false,
+            ])
+            ->add('hasLyrics', CheckboxType::class, [
+                'label' => 'Paroles disponibles ?',
+                'required' => false,
+            ])
             ->add('person', EntityType::class, [
                 'class' => Person::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',  // Utiliser le nom de l'interprète au lieu de l'ID
                 'multiple' => true,
+                'expanded' => false, // Utiliser un select multiple au lieu de checkboxes
+                'label' => 'Interprètes',
+                'attr' => ['class' => 'form-select'],
             ])
             ->add('userSongKnowledge', EntityType::class, [
                 'class' => UserSongKnowledge::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',  // Utiliser un label plus lisible
+                'placeholder' => 'Sélectionner une connaissance',
+                'label' => 'Connaissance utilisateur',
+                'attr' => ['class' => 'form-select'],
             ])
-        ;
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => ['class' => 'px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
