0bfef962c68eaa3afe1f59844dda1c7ea864b7fe
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-29 09:25:18 +0100

    ongoing

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PersonRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PersonController extends AbstractController
 {
@@ -14,5 +17,19 @@ final class PersonController extends AbstractController
         return $this->render('person/index.html.twig', [
             'controller_name' => 'PersonController',
         ]);
+    }
+
+    #[Route('/search/person', name: 'search_person', methods: ['GET'])]
+    public function search(Request $request, PersonRepository $personRepository): JsonResponse
+    {
+        $query = $request->query->get('q');
+        $persons = $personRepository->createQueryBuilder('p')
+            ->where('p.name LIKE :name')
+            ->setParameter('name', "%$query%")
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+
+        return $this->json(array_map(fn($p) => ['id' => $p->getId(), 'text' => $p->getName()], $persons));
     }
 }
